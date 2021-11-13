b043cc3cdfe707b4eed6d379926a46a144994360
Myriamchtf <91917709+Myriamchtf@users.noreply.github.com>
2021-11-13 18:57:13 +0100

    Update Panel.java

--- Panel.java
+++ Panel.java
@@ -1,5 +1,8 @@
+package dauphine;
 
+import java.util.AbstractList;
 import java.util.Iterator;
+import java.util.List;
 
 
 public class Panel {
@@ -8,10 +11,8 @@ public class Panel {
 		// TODO Auto-generated constructor stub
 	}
 	
-	
 	private static Iterator<Integer> panel1(int i, int j) {
 
-		
 		Iterator<Integer> it = new Iterator<Integer>() {
 
 			private int debut = i;
@@ -41,15 +42,72 @@ public class Panel {
 		};
 		return it;
 	}
+	
+	public static Iterable<Integer> panel2(int i, int j) {
+		// TODO Auto-generated method stub
+		
+		Iterable<Integer> it = new Iterable<Integer>() {
+			
+			@Override
+			public Iterator<Integer> iterator() {
+				return panel1(i, j);
+			}
+		};
+		
+		return it;
+	}
+	
+	//Implémentation des méthodes get() et size()
+	
+	public static List<Integer> panel(int i, int j){
+		
+		List<Integer> it = new AbstractList<Integer>() {
+
+			@Override
+			public Integer get(int index) {
+				// TODO Auto-generated method stub
+				return i + index ;
+			}
+
+			@Override
+			public int size() {
+				// TODO Auto-generated method stub
+				return (i-j)+1;
+			}
+			
+			@Override
+			public Iterator<Integer> iterator() {
+				return panel1(i, j);
+			}
+			
+		};
+
+		return it;
+		
+	}
+	
 
 
 	public static void main(String[] args) {
 	
+		System.out.println("Panel 1"); 
 		Iterator<Integer> it=panel1(1,5);
 		for(;it.hasNext();)
 		   System.out.println(it.next()); // affiche 1 2 3 4 5
 		
+		System.out.println("\nPanel 2"); 
+		for(int i:panel2(1,5))
+			System.out.println(i); // affiche 1 2 3 4 5
+		
+		System.out.println("\nPanel 3"); 
+		List<Integer> l = panel(3,6);
+		for (int i:l) {
+			System.out.println(i); // affiche 3 4 5 6 
+		}
+		System.out.println(l.get(1)); //affiche 4
 	}
+
+	
 
 	
 }
